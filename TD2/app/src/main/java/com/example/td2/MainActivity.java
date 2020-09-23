8559bff3846e30ab904a165a967f65ce38e1f473
Alexandre <alexandrepiret.ing@gmail.com>
2020-09-23 10:48:39 +0200

    Modif repas

--- TD2/app/src/main/java/com/example/td2/MainActivity.java
+++ TD2/app/src/main/java/com/example/td2/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.td2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button annuler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.repas);
+
+        annuler = (Button) findViewById(R.id.cancel);
     }
 }
