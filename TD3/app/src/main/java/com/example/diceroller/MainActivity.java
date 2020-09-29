6c7e05f56b533e53d3fa0ea15f6376c9b43074af
Alexandre <alexandrepiret.ing@gmail.com>
2020-09-29 13:50:31 +0200

    Click

--- TD3/app/src/main/java/com/example/diceroller/MainActivity.java
+++ TD3/app/src/main/java/com/example/diceroller/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,11 +18,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button roll = (Button) findViewById(R.id.roll_button);
+        final TextView roll_TextView = (TextView) findViewById(R.id.TextView_Roll);
+
+
         roll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast toast = Toast.makeText(MainActivity.this, "Dé lancé", Toast.LENGTH_SHORT);
-                toast.show();
+                int min = 1;
+                int max = 6;
+
+                Random r = new Random();
+                int myRoll = r.nextInt(max-min+1)+min;
+
+                roll_TextView.setText(myRoll);
             }
         });
     }
