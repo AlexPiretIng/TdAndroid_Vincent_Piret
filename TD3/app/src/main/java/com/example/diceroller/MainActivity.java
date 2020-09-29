575f2d564eb71e73641cd41fc54ebb11fd83bbfa
Alexandre <alexandrepiret.ing@gmail.com>
2020-09-29 13:57:31 +0200

    Return roll

--- TD3/app/src/main/java/com/example/diceroller/MainActivity.java
+++ TD3/app/src/main/java/com/example/diceroller/MainActivity.java
@@ -20,19 +20,24 @@ public class MainActivity extends AppCompatActivity {
         Button roll = (Button) findViewById(R.id.roll_button);
         final TextView roll_TextView = (TextView) findViewById(R.id.TextView_Roll);
 
-
         roll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int min = 1;
-                int max = 6;
-
-                Random r = new Random();
-                int myRoll = r.nextInt(max-min+1)+min;
-
-                roll_TextView.setText(myRoll);
+                roll_TextView.setText(Random());
             }
         });
+    }
+
+    public String Random(){
+        int roll=0;
+        int min = 1;
+        int max = 6;
+
+        Random r = new Random();
+        roll = r.nextInt(max-min+1)+min;
+        String res = String.valueOf(roll);
+        return res;
+
     }
 
 }
