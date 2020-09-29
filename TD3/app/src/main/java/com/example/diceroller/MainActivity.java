78115d487913d209f254fc0f6ed38649f99d05aa
Erwan <erwan-vincent@protonmail.com>
2020-09-29 13:42:54 +0200

    Add : Button change label

--- TD3/app/src/main/java/com/example/diceroller/MainActivity.java
+++ TD3/app/src/main/java/com/example/diceroller/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.diceroller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button roll = (Button) findViewById(R.id.roll_button);
+        roll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(MainActivity.this, "Dé lancé", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
     }
+
 }
