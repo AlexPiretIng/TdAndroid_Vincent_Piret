6918841518b9d65603e0d229f7ae200285944f4b
Erwan <erwan-vincent@protonmail.com>
2020-10-20 14:29:40 +0200

    THE good project

--- TD52/app/src/main/java/com/example/td5/ContactActivity.java
+++ TD52/app/src/main/java/com/example/td5/ContactActivity.java
@@ -1,14 +1,37 @@
 package com.example.td5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContactActivity extends AppCompatActivity {
+
+    List<Contact> contacts = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView rvContacts = (RecyclerView)findViewById(R.id.rvContacts);
+
+        contacts.add(new Contact("Jean","Pierre"));
+        contacts.add(new Contact("Jeanne","D'arc"));
+        contacts.add(new Contact("Pierre","Menez"));
+        contacts.add(new Contact("Arthur","Rimbaut"));
+        contacts.add(new Contact("Richard","Coeur de lion"));
+        contacts.add(new Contact("Zinedine","Zidane"));
+        contacts.add(new Contact("Yannick","Noah"));
+
+        ContactAdapter adapter = new ContactAdapter(contacts);
+
+        rvContacts.setAdapter(adapter);
+
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
     }
 }
