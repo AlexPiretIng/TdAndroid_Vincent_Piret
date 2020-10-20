d5c6f98924ce3447d46263f5f3e44d31ef957cfd
Erwan <erwan-vincent@protonmail.com>
2020-10-20 13:57:48 +0200

    ADD : Complete methods

--- TD52/app/src/main/java/com/example/td5/ContactAdapter.java
+++ TD52/app/src/main/java/com/example/td5/ContactAdapter.java
@@ -1,5 +1,7 @@
 package com.example.td5;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -20,18 +22,27 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
 
-
+        View contactView = inflater.inflate(R.layout.item_contact, parent, false);
+        return new ViewHolder(contactView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Contact contact = mContat.get(position);
 
+        TextView firstNameTextView = holder.firstNameTextView;
+        firstNameTextView.setText(contact.getPrenom());
+
+        TextView lastNameTextView = holder.lastNameTextView;
+        lastNameTextView.setText(contact.getNom());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mContat.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
