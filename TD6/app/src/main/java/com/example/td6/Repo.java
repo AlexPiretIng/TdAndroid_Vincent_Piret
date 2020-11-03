35710e120f0b9fc8dcc97143197f25389130b4c9
Erwan <erwan-vincent@protonmail.com>
2020-11-03 16:52:35 +0100

    Retrofit et adapter

---
+++ TD6/app/src/main/java/com/example/td6/Repo.java
@@ -0,0 +1,41 @@
+package com.example.td6;
+
+public class Repo {
+
+    private int id;
+    private String name;
+    private String fullname;
+    private String html_url;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getHtml_url() {
+        return html_url;
+    }
+
+    public void setHtml_url(String html_url) {
+        this.html_url = html_url;
+    }
+}
