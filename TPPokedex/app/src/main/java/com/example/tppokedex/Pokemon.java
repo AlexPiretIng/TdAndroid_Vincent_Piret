17ab7da82d18b80d828be7eeab38fdf22c7c1d40
Alex <alexandrepiret.ing@gmail.com>
2020-12-09 14:53:31 +0100

    Interface pokemon et classes

---
+++ TPPokedex/app/src/main/java/com/example/tppokedex/Pokemon.java
@@ -0,0 +1,32 @@
+package com.example.tppokedex;
+
+public class Pokemon {
+
+    private String name;
+    private String url;
+    private int number;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
